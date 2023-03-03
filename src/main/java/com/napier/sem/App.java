e0dcc105201d893cb32fca15b3d6de970413adcb
josephm89 <joseph.meszaros89@yahoo.co.uk>
2023-03-03 02:02:05 +0000

    update main function to display employee data

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -123,6 +123,10 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
